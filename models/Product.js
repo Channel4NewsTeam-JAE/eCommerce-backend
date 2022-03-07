d328529125830e427c6707990c82f504cb06e8cc
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-07 16:46:13 -0600

    added category and quanity fields to product model

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,8 @@ const mongoose = require('./../db/connection')
 
 const productSchema = new mongoose.Schema({
     name: String,
-    price: Number 
+    price: Number,
+    quantity: Number 
 })
 
 module.exports = mongoose.model('Product', productSchema)
