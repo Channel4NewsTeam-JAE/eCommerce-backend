cd334852597b51823220989f22379bd5d694e472
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-07 17:32:33 -0600

    adjusted Product model to have category field

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,8 @@ const mongoose = require('./../db/connection')
 const productSchema = new mongoose.Schema({
     name: String,
     price: Number,
-    quantity: Number 
+    quantity: Number,
+    category: String 
 })
 
 module.exports = mongoose.model('Product', productSchema)
