657f4f35acfafcfde68cae7ed5a6369bf9a4de8e
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-08 15:39:25 -0600

    fixed localhost address

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
-    origin: `http://localhost:3000`,  //react's address
+    origin: `http://localhost:3001`,  //react's address
     credentials: true
 }));
 
