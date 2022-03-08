bea6d45c2cd819cd4c65763346802f971ba7b20a
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-08 13:23:18 -0600

    added DB_URL to connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 let mongooseURL = ""
 
 if (process.env.NODE_ENV === "production") {
-  mongooseURL = ""
+  mongooseURL = process.env.DB_URL
 } else {
   mongooseURL = "mongodb://127.0.0.1/eCommerce-backend"
 }
