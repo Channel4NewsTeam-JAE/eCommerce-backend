184e286b5015fa79bcdffe8bec88eea106530d90
jasonhensley1989 <jasonhensley1989@gmail.com>
2022-03-08 10:47:24 -0800

    relative path update

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Seller = require('../models/Seller');
+const Seller = require('./sellerController');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
