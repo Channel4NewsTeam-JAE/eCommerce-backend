3acd0bf7400d3dc715b22cf4afb5d28927efb512
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-08 11:53:44 -0600

    added post route for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const Product = require('./../models/Product.js');
+const Product = require('./../models/Product');
 const router = express.Router();
+
 
 //get route
 router.get("/", async (req, res) => {
@@ -11,33 +12,25 @@ router.get("/", async (req, res) => {
     });
   });
 
-
-// create product
-
-// router.post("/", async (req, res) => {
-//   const data = req.body;
-//   const newProduct = await Seller.create(data);
-//   res.json({
-//     status: 200,
-//     data: newProduct,
-//   });
-// });
-
 //delete
-// router.delete('/:id', async (req, res) => {
-//     const product = await Product.findByIdAndDelete(req.params.id)
-//     res.json({
-//       status: 200,
-//       msg: `${product} was deleted from the list!`
-//     })
-//   })
+router.delete('/:id', async (req, res) => {
+    const product = await Product.findByIdAndDelete(req.params.id)
+    res.json({
+      status: 200,
+      msg: `${product} was deleted from the list!`
+    })
+  })
 
-//update product
-// router.put('/:id', async (req, res) => {
-//     const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
-//     res.json({
-//       status: 200,
-//       msg: `Update: ${product}} was updated in the database`
-//     })
-//   })
+// post
+router.post("/", async (req, res) => {
+  const data = req.body;
+  console.log("DATA: ", data)
+  const product = await Product.create(data);
+  res.json({
+    status: 200,
+    data: product,
+  });
+  console.log(product)
+});
+
 module.exports = router;
