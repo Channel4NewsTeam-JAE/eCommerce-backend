600b9f8e0a7f1b4a2bdf4f41ad1a587563af0f32
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-07 17:09:22 -0700

    Updated Product model to include description property

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,7 @@ const mongoose = require('./../db/connection')
 const productSchema = new mongoose.Schema({
     name: String,
     price: Number,
+    description: String,
     quantity: Number,
     category: String 
 })
