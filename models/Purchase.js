40921a51b6b990336b9d6352ff8b7cb39f3406a5
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-08 15:28:09 -0700

    Modified products property to be an array of products.

--- models/Purchase.js
+++ models/Purchase.js
@@ -3,7 +3,7 @@ const mongoose = require('./../db/connection')
 const purchaseSchema = new mongoose.Schema({
     totalPrice: Number,
     paid: Boolean,
-    products: {type: mongoose.Schema.Types.ObjectId, ref:'Product'}
+    products: [{type: mongoose.Schema.Types.ObjectId, ref:'Product'}]
 })
 
 module.exports = mongoose.model('Purchase', purchaseSchema)
