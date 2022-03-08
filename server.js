82009d1959622146097e502ec5d77e1d653faabc
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-07 18:35:58 -0600

    added routes for controllers [product,purchases,sellers] in server

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const app = express()
 // app.use(logger('dev'));
 app.use(cors())
 
+const productController = require('./controllers/productController')
+app.use('/products', productController)
+
+const purchaseController = require('./controllers/purchaseController')
+app.use('/purchases', purchaseController)
+
+const sellerController = require('./controllers/sellerController')
+app.use('/sellers', sellerController)
+
+
 app.get('/', function (req, res) {
     res.send('hello, earth!')
 })
